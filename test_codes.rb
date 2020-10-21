19c8ca47efdfe2f7bdfd9ce85d36d99692b1be70
sumraSquare63 <43162664+sumraSquare63@users.noreply.github.com>
2020-10-21 22:15:22 +0500

    Update test_codes.rb

--- test_codes.rb
+++ test_codes.rb
@@ -1,1 +1,38 @@
 
+  
+Task 1
+You are to write a migration for users table to add new columns `first_name, last_name, job_title, birth_date`. All but `birth_date` is a datetime column which will include timezone
+sol:
+class AddCilumnsToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :job_title, :string
+    add_column :users, :birth_date, :datetime
+  end
+end
+
+  Task 2
+Create a new model called `PhoneNumber` which should include the following properties. 
+
+id: type: integer
+number: type: integer
+country: type: integer
+isMobile: type: boolean
+createdAt: type: datetime
+updatedAt: type: datetime
+
+sol: rails generate model PhoneNumber number:integer country:inetger isMobile:boolean 
+  
+  
+Task 3
+Create many-to-many relationships for `Users` model with `PhoneNumbers` model
+
+  
+  task 4
+  Create CRUD for Users
+
+  sol: rails generate contorller Users
+    
+    
+ 
